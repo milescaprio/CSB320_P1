fb2c8b916ac94b8c88f8996d317e60848d3ffb9f
Miles Caprio <46407361+milescaprio@users.noreply.github.com>
2025-05-16 01:13:35 -0700

    Project 1 V2

---
+++ remove_warnings.py
@@ -0,0 +1,17 @@
+import sys
+import os
+import warnings
+from contextlib import contextmanager
+
+@contextmanager
+def suppress_stdout_stderr():
+    with open(os.devnull, 'w') as devnull:
+        old_stdout = sys.stdout
+        old_stderr = sys.stderr
+        sys.stdout = devnull
+        sys.stderr = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
+            sys.stderr = old_stderr
